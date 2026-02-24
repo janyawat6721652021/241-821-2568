9bb5b29543427c6c8a875028d61cf78c1d07c667
janyawat6721652021 <janyawat.ta@ku.th>
2026-02-24 12:37:42 +0700

    update api express

--- Backend/index.js
+++ Backend/index.js
@@ -1,63 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql2/promise');
 const app = express();
 const port = 8000;
-
 app.use(bodyParser.json());
-
 let users = []
 let counter = 1;
 
-//path = GET /users
-app.get('/users',  (req, res) => {
-    res.json(users);
+let conn = null
+const initDbConnection = async () => {
+    conn = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'webdb',
+        port: 8821
+    })
+}
+
+// path = GET users สำหรับ get ข้อมูล users ทั้งหมด
+app.get('/users', async (req, res) => {
+    const results = await conn.query('SELECT * FROM users');
+    res.json(results[0]);
+})
+
+app.listen(port, async () => {
+    await initDbConnection();
+    console.log(`Server is running at http://localhost:${port}`);
 });
-
-//path = POST /user
-app.post('/users', (req, res) => {
-    let user = req.body;
-    user.id = counter
-    counter += 1
-    users.push(user);
-    res.json({ 
-        message: 'User added successfully', 
-        user: user });
-})
-
-//path = PUT /user/:id
-app.patch('/users/:id', (req, res) => {
-    let id = req.params.id
-    let updatedUser = req.body;
-    // หา users จาก id
-    let selectedIndex = users.findIndex(user => user.id == id)
-    //update users นั้น
-    if (updatedUser.name) {
-        users[selectedIndex].name = updatedUser.name    
-    }
-    if (updatedUser.age) {
-        users[selectedIndex].age = updatedUser.age
-    }
-
-    //ส่ง response กลับไปว่า update users ที่เลือกสำเร็จแล้ว
-
-    res.json({
-        message: 'User updated successfully',
-        data : {
-            user: updatedUser,
-            indexUpdated: selectedIndex
-        }
-    })  
-})
-
-//path = DELETE /users/:id
-app.delete('/users/:id', (req, res) => {
-    let id = req.params.id
-    let selectedIndex = users.findIndex(user => user.id == id)
-    if (selectedIndex !== -1) {
-        users.splice(selectedIndex, 1)
-        res.json({
-            message: 'User deleted successfully',
-            data: {
-                indexDeleted: selectedIndex
-            }
-        })
