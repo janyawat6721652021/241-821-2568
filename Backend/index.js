0cf03d7f5bd3898d57b3d9fe432aa8af772f4cae
janyawat6721652021 <janyawat.ta@ku.th>
2026-02-28 11:47:31 +0700

    update api express

--- Backend/index.js
+++ Backend/index.js
@@ -18,6 +18,26 @@ const initDbConnection = async () => {
     })
 }
 
+// path = POST users สำหรับสร้าง user ใหม่
+app.post('/users', async (req, res) => {
+    try {
+        let user = req.body; 
+        const [results] = await conn.query('INSERT INTO users SET ?', user); 
+        
+        res.json({
+            message: 'User created successfully',
+            data: results 
+        });
+    } catch (error) {
+        console.error('Error creating user:', error);
+        res.status(500).json({
+            message: 'Error creating user',
+            error: error.message
+        });
+    }
+}); 
+
+
 // path = GET users สำหรับ get ข้อมูล users ทั้งหมด
 app.get('/users', async (req, res) => {
     const results = await conn.query('SELECT * FROM users');
@@ -27,4 +47,45 @@ app.get('/users', async (req, res) => {
 app.listen(port, async () => {
     await initDbConnection();
     console.log(`Server is running at http://localhost:${port}`);
+});
+// path = GET users/:id สำหรับ get ข้อมูล user ตาม id
+app.get('/users/:id', async (req, res) => {
+    try { 
+        const userId = req.params.id;
+        const results = await conn.query('SELECT * FROM users WHERE id = ?', [userId]);
+        if (results[0].length === 0) {
+            throw { statusCode: 404, message: 'User not found' }; 
+        }
+
+        res.json(results[0][0]);
+    } catch (error) { 
+        console.error('Error fetching user:', error.message);
+        let statusCode = error.statusCode || 500; 
+        
+        res.status(statusCode).json({
+            message: error.message || 'Error fetching user',
+            error: error.message 
+        });
+    }
+});
+
+// PUT /users/:id สำหรับแก้ไขข้อมูล user ตาม id 
+app.put('/users/:id', async (req, res) => {
+    try {
+        let updatedUser = req.body;
+        const results =await conn.query('UPDATE users SET ? WHERE id = ?', [updatedUser, req.params.id]);
+        if (results[0].affectedRows === 0) {
+            throw { statusCode: 404, message: 'User not found'};
+        }
+        res.json({
+            message: 'User updated successfully',
+            data: results[0]
+        });
+    } catch (error) {
+        let statusCode = error.statusCode || 500;
+        res.status(statusCode).json({
+            message: error.message || 'Error updating user',
+            error: error.message
+        });
+    }
 });
