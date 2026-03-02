7302661be28b88ed059f5de652f996e3b88e43f0
janyawat6721652021 <janyawat.ta@ku.th>
2026-03-02 17:16:06 +0700

    update frontend

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,11 +1,13 @@
-function submitData(){
+const submitData = async () => {
     let firstNameDOM = document.querySelector('input[name="firstname"]');
     let lastNameDOM = document.querySelector('input[name="lastname"]');
     let ageDOM = document.querySelector('input[name="age"]');
     let genderDOM = document.querySelector('input[name="gender"]:checked');
-    let interestDOMs = document.querySelectorAll('input[name="interests"]:checked');
+    let interestDOMs = document.querySelectorAll('input[name=interests]:checked');
     let descriptionDOM = document.querySelector('textarea[name="description"]');
 
+    let messageDOM = document.getElementById('message');
+try {
     let interest = ''
     for (let i=0; i < interestDOMs.length; i++){
         interest += interestDOMs[i].value 
@@ -14,14 +16,24 @@ function submitData(){
         }
     }
 
-    let userData ={
-        firstname: firstNameDOM.value,
-        lastname: lastNameDOM.value,
-        age: ageDOM.value,
-        gender: genderDOM.value,
-        description: descriptionDOM.value,
-        interest: interest
+        let userData ={
+            firstname: firstNameDOM.value,
+            lastname: lastNameDOM.value,
+            age: ageDOM.value,
+            gender: genderDOM.value,
+            description: descriptionDOM.value,
+            interests: interest
+        }
+        const response = await axios.post("http://localhost:8000/users", userData)
+        console.log('response', response.data);
+        messageDOM.innerText = 'Data Saved Successfully!'
+        messageDOM.className = 'message success'
+    } catch (error) {
+        if (error.response) {
+            console.log('Error response:', error.response.data);
+        }
+        messageDOM.innerText = 'Error saving data! ' 
+        messageDOM.className = 'message danger';
     }
-    console.log('submit data: ', userData);
-
+    console.log('submitData', userData);
 }
